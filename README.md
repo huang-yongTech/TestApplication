610860b5c54d8fdfb1f1b633c8702ef228662d54
huangyong <756655135@qq.com>
2018-12-29 15:17:44 +0800

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # TestApplication
+
+学习测试项目application，
+主要用于RxJava2、Retrofit2以及OkHttp3等源码分析用
---
+++ app/src/main/java/com/test/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.test.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.test.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
