3b1b07886fa2dcee9607a67e6f0e3a1bead228e9
huangyong <756655135@qq.com>
2018-12-29 19:40:02 +0800

    事件订阅

--- app/src/main/java/com/test/activity/MainActivity.java
+++ app/src/main/java/com/test/activity/MainActivity.java
@@ -3,10 +3,18 @@ package com.test.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 import com.test.R;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +22,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        init();
+    }
+
+    private void init() {
+        Observer<Integer> observer = new Observer<Integer>() {
+            Disposable disposable;
+
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.i(TAG, "onSubscribe:");
+                disposable = d;
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.i(TAG, "onNext: " + integer);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.i(TAG, "onError: " + e.getMessage());
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onComplete() {
+                Log.i(TAG, "onComplete:");
+            }
+        };
+
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) {
+                emitter.onNext(1);
+                emitter.onNext(2);
+                emitter.onNext(3);
+                emitter.onComplete();
+            }
+        }).subscribe(observer);
     }
 }
