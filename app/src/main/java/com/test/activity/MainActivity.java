aa55a5fd1a9f83b15a9e77dccfcfffb761a18b12
huangyong <756655135@qq.com>
2019-01-04 19:24:39 +0800

    订阅线程切换

--- app/src/main/java/com/test/activity/MainActivity.java
+++ app/src/main/java/com/test/activity/MainActivity.java
@@ -7,38 +7,49 @@ import android.util.Log;
 
 import com.test.R;
 
+import java.util.concurrent.TimeUnit;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+
+    private Disposable disposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
+
+        mToolbar.setTitle("");
+        setSupportActionBar(mToolbar);
 
         init();
     }
 
     private void init() {
         Observer<Integer> observer = new Observer<Integer>() {
-            Disposable disposable;
-
             @Override
             public void onSubscribe(Disposable d) {
-                Log.i(TAG, "onSubscribe:");
+                Log.i(TAG, "onSubscribe--运行线程：" + Thread.currentThread().getName());
                 disposable = d;
             }
 
             @Override
             public void onNext(Integer integer) {
-                Log.i(TAG, "onNext: " + integer);
+                Log.i(TAG, "onNext: " + integer + " --运行线程：" + Thread.currentThread().getName());
+                mToolbar.setTitle(integer + "");
             }
 
             @Override
@@ -49,18 +60,26 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onComplete() {
-                Log.i(TAG, "onComplete:");
+                Log.i(TAG, "onComplete--运行线程：" + Thread.currentThread().getName());
             }
         };
 
         Observable.create(new ObservableOnSubscribe<Integer>() {
             @Override
             public void subscribe(ObservableEmitter<Integer> emitter) {
+                Log.i(TAG, "subscribe--运行线程：" + Thread.currentThread().getName());
                 emitter.onNext(1);
                 emitter.onNext(2);
+                try {
+                    TimeUnit.MILLISECONDS.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 emitter.onNext(3);
                 emitter.onComplete();
             }
-        }).subscribe(observer);
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(observer);
     }
 }
